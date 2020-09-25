212612c4da0923c7d38a7e726c6ef2c3116fdb24
Drevok <theoartous@hotmail.fr>
2020-09-25 09:59:05 +0200

    Added basic assets and started the enemy script

---
+++ Assets/Scripts/ENEMY.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemies
+{
+    public class ENEMY : MonoBehaviour
+    {
+        public abstract class Enemy
+        {
+            protected int MoveSpeed;
+            protected int MaxHealth;
+            protected float CurrentHealth;
+
+            protected void Moving ()
+            {
+
+            }
+        }
+    }
+}
