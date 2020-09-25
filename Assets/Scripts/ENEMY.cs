5b39ee94fc27938026f66d7b6a7e56ffe82bf3fc
Drevok <theoartous@hotmail.fr>
2020-09-25 10:12:06 +0200

    Continued the pathfinding script

--- Assets/Scripts/ENEMY.cs
+++ Assets/Scripts/ENEMY.cs
@@ -9,13 +9,26 @@ namespace Enemies
     {
         public abstract class Enemy
         {
-            protected int MoveSpeed;
-            protected int MaxHealth;
-            protected float CurrentHealth;
+            protected static int MoveSpeed;
+            protected static int MaxHealth;
+            protected static float CurrentHealth;
+            protected static Vector3 destination;
+            protected static Transform target;
+            protected static NavMeshAgent agent;
 
             protected void Moving ()
             {
+                if (Vector3.Distance(destination, target.position) > 1.0f)
+                {
+                    destination = target.position;
+                    agent.destination = destination;
+                }                
+            }
 
+            void Start()
+            {
+                agent = GetComponent<NavMeshAgent>();
+                Goal = agent.Goal; 
             }
         }
     }
