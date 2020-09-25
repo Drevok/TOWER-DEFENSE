ebe5cea062e56fa4c9b165382164db9ab0157fbb
Drevok <theoartous@hotmail.fr>
2020-09-25 17:27:40 +0200

    WORKING IN AUTOMATIC!

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -17,16 +17,9 @@ namespace Enemies
             NavMeshAgent = GetComponent<NavMeshAgent>();
         }
 
-        private void Update() {
-            if (Input.GetButtonDown("Fire1"))
-            {
-                //RaycastHit hit;
-                //Ray ray = Camera.ScreenPointToRay(Input.mousePosition);
-                //if (Physics.Raycast(ray, out hit))
-                //{
-                    testenemy.Moving(this, Goal.transform.position);
-                //}
-            }
+        private void Start() 
+        {
+         testenemy.Moving(this, Goal.transform.position);
         }
     }
 }
