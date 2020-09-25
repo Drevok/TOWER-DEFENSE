9cc555b455b174eed387be6d199a1802352df6af
Drevok <theoartous@hotmail.fr>
2020-09-25 12:19:10 +0200

    Continued fixing of navmesh (still not finished yet)

---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Enemies
+{
+    public class EnemyScript : MonoBehaviour
+    {
+        public ENEMY testenemy;
+        void Start ()
+        {
+
+        }
+    }
+}
