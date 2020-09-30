67c371f7b9cfc7f6219dd6fcc6b89e95d65620e5
Drevok <71374058+Drevok@users.noreply.github.com>
2020-09-30 22:26:22 +0200

    Created other child

--- Assets/Scripts/NormalEnemy.cs
+++ Assets/Scripts/NormalEnemy.cs
@@ -21,4 +21,20 @@ namespace Enemies
             
         }  
     }
+
+    [CreateAssetMenu(fileName ="New Enemy", menuName = "Fast Enemy")]
+
+    public class FastEnemy : ENEMY
+    {
+        public override void Moving(EnemyScript enemyScript, Vector3 position)
+        {
+            //enemyScript.NavMeshAgentspeed = Random.value * MoveSpeed;
+            enemyScript.NavMeshAgent.SetDestination(position);
+        }
+
+        public override void TakeDamage(EnemyScript enemy)
+        {
+
+        }
+    }
 }
