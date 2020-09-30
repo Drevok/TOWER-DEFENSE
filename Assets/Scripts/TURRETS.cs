a7efd1ef19c30825d0089894be6e951924238483
Drevok <theoartous@hotmail.fr>
2020-09-30 10:28:58 +0200

    Turrets can detect enemies

--- Assets/Scripts/TURRETS.cs
+++ Assets/Scripts/TURRETS.cs
@@ -7,14 +7,40 @@ public class TURRETS : MonoBehaviour
     public Transform target;
     public float range = 10f;
 
+    public string enemyTag = "Enemy";
+
     void Start()
     {
-        
+        InvokeRepeating("UpdateTarget", 0f, 0.2f);
+    }
+
+    void UpdateTarget ()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if(distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+
     }
 
     void Update()
     {
-        
+        if (target == null)
+            return;
     }
 
     private void OnDrawGizmosSelected()
