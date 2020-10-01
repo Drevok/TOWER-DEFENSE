28e2944a105c47fa98f4c813058e478cfe8977d9
Drevok <71374058+Drevok@users.noreply.github.com>
2020-10-01 11:38:23 +0200

    Created Build Manager (and finished it?)

---
+++ Assets/BuildManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+   
+
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManager");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    private GameObject turretToBuild;
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+    void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+}
--- Assets/ConstructionScript.cs
+++ Assets/ConstructionScript.cs
@@ -6,13 +6,28 @@ public class ConstructionScript : MonoBehaviour
 {
     public Color hoverColor;
 
+    private GameObject turret;
+
     private Renderer rend;
     private Color startColor;
+
 
     void Start()
     {
         rend = GetComponent<Renderer>();
         startColor = rend.material.color;
+    }
+
+    void OnMouseDown()
+    {
+        if (turret != null)
+        {
+            Debug.Log("Can't build there! - TODO: Display on screen");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = (GameObject)Instantiate(turretToBuild, transform.position, transform.rotation);
     }
 
     void OnMouseEnter()
